f52eabd429f813716c2596709c6eca100e63d97f
Alex Shapiro <alex@alexshapiro.xyz>
2026-03-16 16:34:53 -0400

    remove ayml-serde dependency on ayml-core

--- ayml-serde/src/error.rs
+++ ayml-serde/src/error.rs
@@ -3,8 +3,6 @@ use std::fmt;
 /// Error type for ayml-serde serialization and deserialization.
 #[derive(Debug)]
 pub enum Error {
-    /// An error from the ayml-core parser.
-    Parse(ayml_core::Error),
     /// A custom message from serde or this crate.
     Message(String),
     /// An I/O error (from `from_reader` / `to_writer`).
@@ -14,7 +12,6 @@ pub enum Error {
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Error::Parse(e) => write!(f, "{e}"),
             Error::Message(msg) => f.write_str(msg),
             Error::Io(e) => write!(f, "I/O error: {e}"),
         }
@@ -24,7 +21,6 @@ impl fmt::Display for Error {
 impl std::error::Error for Error {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
-            Error::Parse(e) => Some(e),
             Error::Message(_) => None,
             Error::Io(e) => Some(e),
         }
@@ -40,12 +36,6 @@ impl serde::de::Error for Error {
 impl serde::ser::Error for Error {
     fn custom<T: fmt::Display>(msg: T) -> Self {
         Error::Message(msg.to_string())
-    }
-}
-
-impl From<ayml_core::Error> for Error {
-    fn from(e: ayml_core::Error) -> Self {
-        Error::Parse(e)
     }
 }
 
